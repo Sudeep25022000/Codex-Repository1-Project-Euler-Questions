673434c25d009b0253eafad5f5baebd94cde7d23
Sudeep Swain <53346605+Sudeep25022000@users.noreply.github.com>
2019-07-29 20:04:26 +0530

    Update codex9.c

--- codex9.c
+++ codex9.c
@@ -6,19 +6,18 @@ void main()
     int j;
     int k;
     int flag;
-    for(i=0;i<1000/2;i++)
+    for(i=3;i<(1000-3)/2;i++)
     {
-        for(j=i+1;j<1000/2;j++)
+        for(j=i+1;j<(1000-i-1)/2;j++)
         {
-            for(k=j+1;k<1000/2;k++)
-            {
+                k=1000-i-j;
                 if((i*i)+(j*j)==(k*k) && (i+j+k==1000))
                 {
                     int product=i*j*k;
                     printf("%d, %d, %d\n",i,j,k);
                     printf("\n%d",product);
                 }
-            }
+
         }
     }
 
@@ -29,4 +28,3 @@ void main()
     }
     getch();
 }
-
