a0665a96284ffd7d54fd4bf9e59693dbf55b8382
Sudeep Swain <53346605+Sudeep25022000@users.noreply.github.com>
2019-07-30 09:57:42 +0530

    Update codex7.c

--- codex7.c
+++ codex7.c
@@ -3,17 +3,20 @@
 void main()
 {
    int count;
-   int j=3;
+   int j=5;
    int check;
 
-   for(count=2;count<=10001;)
+   for(count=3;count<=10001;)
    {
       for(check=3;check<=j;check=check+2)
       {
-         if(j%check==0)
+         if(j>5 && j%10==5)
+            break;
+
+         else if(j%check==0)
             break;
       }
-      if (check==j)
+      if(check==j)
       {
          if(count==10001)
          {
